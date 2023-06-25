cfd33b1913a302734894d9c5a3b542edcc4cebd2
Șulic Octavian <octavius1992@gmail.com>
2023-06-25 20:23:49 +0300

    Add project files.

---
+++ AbilityScoreCalculator/AbilityScoreCalculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbilityScoreTester
+{
+    internal class AbilityScoreCalculator
+    {
+        public int RollResult = 14;
+        public double DivideBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score;
+        public void CalculateAbilityScore()
+        {
+            // Divide the roll result by the DivideBy field
+            double divided = RollResult / DivideBy;
+            // Add AddAmount to the result of that division
+            int added = AddAmount += (int)divided;
+
+            // If the result is too small, use Minimum
+            if (added < Minimum)
+            {
+                Score = Minimum;
+            }
+            else
+            {
+                Score = added;
+            }
+        }
+
+        /// <summary>
+        /// Writes a prompt and reads an int value from the console.
+        /// </summary>
+        /// <param name="lastUsedValue">The default value</param>
+        /// <param name="prompt">Prompt to print to the console.</param>
+        /// <returns>The int value read, or the default value if unable to parse</returns>
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.WriteLine($"{prompt} {lastUsedValue}");
+
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int value))
+            {
+                Console.WriteLine(" using value " + value);
+                return value;
+            }
+
+            Console.WriteLine(" using value " + lastUsedValue);
+            return lastUsedValue;
+        }
+    }
+}
---
+++ AbilityScoreCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using AbilityScoreTester;
+
+AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+while (true)
+{
+    calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+
+    calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+
+    calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+
+    calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+
+    calculator.CalculateAbilityScore();
+
+    Console.WriteLine("Calculated ability score: " + calculator.Score);
+
+    Console.WriteLine("Press Q to quit, any other key to continue");
+
+    char keyChar = Console.ReadKey(true).KeyChar;
+
+    if ((keyChar == 'Q') || (keyChar == 'q')) return;
+}
